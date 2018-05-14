726b7f57247571690ee4b27d4ce174d84b2105ba
Daniil Naumetc <daniilnaumetc@Daniils-MBP.fritz.box>
2018-05-14 23:56:42 +0200

    f

--- main.js
+++ main.js
@@ -291,7 +291,7 @@ class T {
       console.log('inlinequery--->', inline_query)
       //ffmpeg -i input.mp3 -c:a libopus output.opus
       
-      ifcb(queryText.match(/moi$/), res => {
+      ifcb(queryText.match(/zhop$/), res => {
         console.log('matched *mine, ', res)
         this.VoicesDb.find({chatId}, (err, voices) => {
           console.log(voices)
