049f38ebab39ad022b6ac7a2be5f8294d572fef0
Daniil Naumetc <daniilnaumetc@Daniils-MBP.fritz.box>
2018-05-25 01:33:39 +0200

    force

--- main.js
+++ main.js
@@ -80,13 +80,13 @@ var Collections = sequelize.define('collections', {
   },
 });
 
-sequelize.sync({ force: false }).then(function(err) {
+sequelize.sync({ force: true }).then(function(err) {
     console.log('It worked!');
   }, function (err) {
     console.log('An error occurred while creating the table:', err);
 });
 
-Voices.sync({force: false}).then(() => {
+Voices.sync({force: true}).then(() => {
   // Table created
   // return Voices.create({
   //   name: 'John',
@@ -95,7 +95,7 @@ Voices.sync({force: false}).then(() => {
 
   console.log('Sync done...')
 });
-Voices.sync({force: false}).then(() => {
+Voices.sync({force: true}).then(() => {
   // Table created
   return Voices.findAll().then(users => {
     console.log(users)
